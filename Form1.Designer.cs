cf86014f61725dffe832deef3189ea73228ceb2e
Jonny Yu <Jonny.worker@gmail.com>
2025-07-03 16:56:26 -0700

    One row button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,83 +53,128 @@
       // 
       // B1_1
       // 
+      this.B1_1.BackColor = System.Drawing.Color.Transparent;
+      this.B1_1.FlatAppearance.BorderSize = 0;
+      //this.B1_1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_1.Location = new System.Drawing.Point(20, 20);
       this.B1_1.Name = "B1_1";
       this.B1_1.Size = new System.Drawing.Size(47, 45);
       this.B1_1.TabIndex = 1;
-      this.B1_1.Text = "button1";
+      this.B1_1.Text = "";
       this.B1_1.UseVisualStyleBackColor = true;
       // 
       // B1_2
       // 
+      this.B1_2.BackColor = System.Drawing.Color.Transparent;
+      this.B1_2.FlatAppearance.BorderSize = 0;
+      //this.B1_2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_2.Location = new System.Drawing.Point(68, 20);
       this.B1_2.Name = "B1_2";
       this.B1_2.Size = new System.Drawing.Size(47, 45);
       this.B1_2.TabIndex = 2;
-      this.B1_2.Text = "button1";
+      this.B1_2.Text = "";
       this.B1_2.UseVisualStyleBackColor = true;
       // 
       // B1_3
       // 
+      this.B1_3.BackColor = System.Drawing.Color.Transparent;
+      this.B1_3.FlatAppearance.BorderSize = 0;
+      //this.B1_3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_3.Location = new System.Drawing.Point(116, 20);
       this.B1_3.Name = "B1_3";
       this.B1_3.Size = new System.Drawing.Size(47, 45);
       this.B1_3.TabIndex = 3;
-      this.B1_3.Text = "button1";
+      this.B1_3.Text = "";
       this.B1_3.UseVisualStyleBackColor = true;
       // 
       // B1_4
       // 
+      this.B1_4.BackColor = System.Drawing.Color.Transparent;
+      this.B1_4.FlatAppearance.BorderSize = 0;
+      //this.B1_4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_4.Location = new System.Drawing.Point(167, 20);
       this.B1_4.Name = "B1_4";
       this.B1_4.Size = new System.Drawing.Size(47, 45);
       this.B1_4.TabIndex = 4;
-      this.B1_4.Text = "button1";
+      this.B1_4.Text = "";
       this.B1_4.UseVisualStyleBackColor = true;
       // 
       // B1_5
       // 
+      this.B1_5.BackColor = System.Drawing.Color.Transparent;
+      this.B1_5.FlatAppearance.BorderSize = 0;
+      //this.B1_5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_5.Location = new System.Drawing.Point(215, 20);
       this.B1_5.Name = "B1_5";
       this.B1_5.Size = new System.Drawing.Size(47, 45);
       this.B1_5.TabIndex = 5;
-      this.B1_5.Text = "button1";
+      this.B1_5.Text = "";
       this.B1_5.UseVisualStyleBackColor = true;
       // 
       // B1_6
       // 
+      this.B1_6.BackColor = System.Drawing.Color.Transparent;
+      this.B1_6.FlatAppearance.BorderSize = 0;
+      //this.B1_6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_6.Location = new System.Drawing.Point(263, 20);
       this.B1_6.Name = "B1_6";
       this.B1_6.Size = new System.Drawing.Size(47, 45);
       this.B1_6.TabIndex = 6;
-      this.B1_6.Text = "button1";
+      this.B1_6.Text = "";
       this.B1_6.UseVisualStyleBackColor = true;
       // 
       // B1_7
       // 
+      this.B1_7.BackColor = System.Drawing.Color.Transparent;
+      this.B1_7.FlatAppearance.BorderSize = 0;
+      //this.B1_7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_7.Location = new System.Drawing.Point(314, 20);
       this.B1_7.Name = "B1_7";
       this.B1_7.Size = new System.Drawing.Size(47, 45);
       this.B1_7.TabIndex = 7;
-      this.B1_7.Text = "button1";
+      this.B1_7.Text = "";
       this.B1_7.UseVisualStyleBackColor = true;
       // 
       // B1_8
       // 
+      this.B1_8.BackColor = System.Drawing.Color.Transparent;
+      this.B1_8.FlatAppearance.BorderSize = 0;
+      //this.B1_8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_8.Location = new System.Drawing.Point(362, 20);
       this.B1_8.Name = "B1_8";
       this.B1_8.Size = new System.Drawing.Size(47, 45);
       this.B1_8.TabIndex = 8;
-      this.B1_8.Text = "button1";
+      this.B1_8.Text = "";
       this.B1_8.UseVisualStyleBackColor = true;
       // 
       // B1_9
       // 
+      this.B1_9.BackColor = System.Drawing.Color.Transparent;
+      this.B1_9.FlatAppearance.BorderSize = 0;
+      //this.B1_9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+      //this.B1_9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+      this.B1_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.B1_9.Location = new System.Drawing.Point(410, 20);
       this.B1_9.Name = "B1_9";
       this.B1_9.Size = new System.Drawing.Size(47, 45);
       this.B1_9.TabIndex = 9;
-      this.B1_9.Text = "button1";
+      this.B1_9.Text = "";
       this.B1_9.UseVisualStyleBackColor = true;
       // 
       // Sudoku
