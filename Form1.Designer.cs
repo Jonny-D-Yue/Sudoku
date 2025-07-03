da681a06023b75942596ec7cf13ff01156767637
Jonny Yu <Jonny.worker@gmail.com>
2025-07-03 16:35:35 -0700

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Sudoku
+{
+  partial class Sudoku
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sudoku));
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.B1_1 = new System.Windows.Forms.Button();
+      this.B1_2 = new System.Windows.Forms.Button();
+      this.B1_3 = new System.Windows.Forms.Button();
+      this.B1_4 = new System.Windows.Forms.Button();
+      this.B1_5 = new System.Windows.Forms.Button();
+      this.B1_6 = new System.Windows.Forms.Button();
+      this.B1_7 = new System.Windows.Forms.Button();
+      this.B1_8 = new System.Windows.Forms.Button();
+      this.B1_9 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+      this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(453, 435);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // B1_1
+      // 
+      this.B1_1.Location = new System.Drawing.Point(20, 20);
+      this.B1_1.Name = "B1_1";
+      this.B1_1.Size = new System.Drawing.Size(47, 45);
+      this.B1_1.TabIndex = 1;
+      this.B1_1.Text = "button1";
+      this.B1_1.UseVisualStyleBackColor = true;
+      // 
+      // B1_2
+      // 
+      this.B1_2.Location = new System.Drawing.Point(68, 20);
+      this.B1_2.Name = "B1_2";
+      this.B1_2.Size = new System.Drawing.Size(47, 45);
+      this.B1_2.TabIndex = 2;
+      this.B1_2.Text = "button1";
+      this.B1_2.UseVisualStyleBackColor = true;
+      // 
+      // B1_3
+      // 
+      this.B1_3.Location = new System.Drawing.Point(116, 20);
+      this.B1_3.Name = "B1_3";
+      this.B1_3.Size = new System.Drawing.Size(47, 45);
+      this.B1_3.TabIndex = 3;
+      this.B1_3.Text = "button1";
+      this.B1_3.UseVisualStyleBackColor = true;
+      // 
+      // B1_4
+      // 
+      this.B1_4.Location = new System.Drawing.Point(167, 20);
+      this.B1_4.Name = "B1_4";
+      this.B1_4.Size = new System.Drawing.Size(47, 45);
+      this.B1_4.TabIndex = 4;
+      this.B1_4.Text = "button1";
+      this.B1_4.UseVisualStyleBackColor = true;
+      // 
+      // B1_5
+      // 
+      this.B1_5.Location = new System.Drawing.Point(215, 20);
+      this.B1_5.Name = "B1_5";
+      this.B1_5.Size = new System.Drawing.Size(47, 45);
+      this.B1_5.TabIndex = 5;
+      this.B1_5.Text = "button1";
+      this.B1_5.UseVisualStyleBackColor = true;
+      // 
+      // B1_6
+      // 
+      this.B1_6.Location = new System.Drawing.Point(263, 20);
+      this.B1_6.Name = "B1_6";
+      this.B1_6.Size = new System.Drawing.Size(47, 45);
+      this.B1_6.TabIndex = 6;
+      this.B1_6.Text = "button1";
+      this.B1_6.UseVisualStyleBackColor = true;
+      // 
+      // B1_7
+      // 
+      this.B1_7.Location = new System.Drawing.Point(314, 20);
+      this.B1_7.Name = "B1_7";
+      this.B1_7.Size = new System.Drawing.Size(47, 45);
+      this.B1_7.TabIndex = 7;
+      this.B1_7.Text = "button1";
+      this.B1_7.UseVisualStyleBackColor = true;
+      // 
+      // B1_8
+      // 
+      this.B1_8.Location = new System.Drawing.Point(362, 20);
+      this.B1_8.Name = "B1_8";
+      this.B1_8.Size = new System.Drawing.Size(47, 45);
+      this.B1_8.TabIndex = 8;
+      this.B1_8.Text = "button1";
+      this.B1_8.UseVisualStyleBackColor = true;
+      // 
+      // B1_9
+      // 
+      this.B1_9.Location = new System.Drawing.Point(410, 20);
+      this.B1_9.Name = "B1_9";
+      this.B1_9.Size = new System.Drawing.Size(47, 45);
+      this.B1_9.TabIndex = 9;
+      this.B1_9.Text = "button1";
+      this.B1_9.UseVisualStyleBackColor = true;
+      // 
+      // Sudoku
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(795, 578);
+      this.Controls.Add(this.B1_9);
+      this.Controls.Add(this.B1_8);
+      this.Controls.Add(this.B1_7);
+      this.Controls.Add(this.B1_6);
+      this.Controls.Add(this.B1_5);
+      this.Controls.Add(this.B1_4);
+      this.Controls.Add(this.B1_3);
+      this.Controls.Add(this.B1_2);
+      this.Controls.Add(this.B1_1);
+      this.Controls.Add(this.pictureBox1);
+      this.Name = "Sudoku";
+      this.Text = "Sudoku";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button B1_1;
+    private System.Windows.Forms.Button B1_2;
+    private System.Windows.Forms.Button B1_3;
+    private System.Windows.Forms.Button B1_4;
+    private System.Windows.Forms.Button B1_5;
+    private System.Windows.Forms.Button B1_6;
+    private System.Windows.Forms.Button B1_7;
+    private System.Windows.Forms.Button B1_8;
+    private System.Windows.Forms.Button B1_9;
+  }
+}
+
